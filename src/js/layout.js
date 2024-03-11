720b8bab640570c24c1ec7fbd5344f24b063ca2c
leosequani <156928045+leosequani@users.noreply.github.com>
2024-03-11 16:53:44 +0000

    final

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
+import { useEffect,useState } from "react";
 
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
@@ -9,27 +10,55 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { AddContact } from "./views/AddContact";
+import { Contacts } from "./views/Contacts";
+
+export const AppContext = React.createContext();
+
 
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+
+	const [userInput, setUserInput] = useState({})
+
+	const [contacts, setContacts] = useState([]);
+	const [currentContact, setCurrentContact] = useState([]);
+
+	const myGetFetch = ()=> {
+		
+		fetch('https://playground.4geeks.com/apis/fake/contact/agenda/Leos')
+        .then(response => {
+          let contentType = response.headers.get("content-type");
+          if (contentType && contentType.includes("application/json")) {
+            return response.json();
+          }
+          throw new TypeError("Sorry, There's no JSON here!");
+        })
+        .then(jsonifiedResponse => { setContacts(jsonifiedResponse) })
+        .catch(error => console.log(error));
+	}
+   
+	useEffect(()=>{
+        myGetFetch()
+    },[])
 	const basename = process.env.BASENAME || "";
 
 	return (
 		<div>
+			<AppContext.Provider value = {{contacts,setContacts,myGetFetch,userInput,setUserInput,currentContact, setCurrentContact}}>
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/addcontact" element={<AddContact />} />
+						<Route path="/contacts" element={<Contacts />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
 				</ScrollToTop>
 			</BrowserRouter>
+			</AppContext.Provider>
 		</div>
 	);
 };
